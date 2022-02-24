d69fc55f74cdc8ed7c80bab189312446ccc6f2a1
SlartiBartFast-art <JekHamond@tutanota.com>
2022-02-24 17:40:06 +0500

    image , README , travis

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
+[![Build Status](https://travis-ci.com/SlartiBartFast-art/job4j_tracker.svg?branch=master)](https://travis-ci.com/SlartiBartFast-art/job4j_tracker)
+
+
 # url_shortcut
 
 
@@ -7,4 +10,86 @@
  
 Сервис работает через REST API.
 
-В приложении будет несколько запросов.
+![Image of Arch](https://github.com/SlartiBartFast-art/job4j_tracker/blob/master/image/Screenshot_1.jpg)
+
+
+Сервис позволяет обеспечить безопасность пользователей на сайте,
+за счет замены обычных ссылок на конвертированные.
+Также проект, позволяет для зарегистрированных в системе сервисом/пользователей производить просмотр
+собрнной статистики посещений страниц различных сайтов,
+
+Это веб-приложение реализует поле сайта с использованием принципа архитектуры RESTfull.
+
+Used technologies
+______________________________________________
+- Java Core
+- JWT
+- Rest API
+- Spring Boot Security
+- Spring Boot Data JPA
+- Spring Boot Web
+- Spring Boot Tomcat
+- Maven
+Travis C.I.
+
+В приложении будет несколько запросов, функционал:
+
+1. Регистрация сайта.
+
+Сервисом могут пользоваться разные сайты. Каждому сайту выдается пару пароль и логин.
+Чтобы зарегистрировать сайт в систему нужно отправить запрос.
+URL
+
+POST /registration
+в случае если пользователь регистрируется впервые
+![Image of Arch](https://github.com/SlartiBartFast-art/job4j_tracker/blob/master/image/Screenshot_2.jpg)
+
+Ответ от сервера, в случае если такой пользователь уже существует
+
+![Image of Arch](https://github.com/SlartiBartFast-art/job4j_tracker/blob/master/image/Screenshot_3.jpg)
+
+2. Авторизация.
+
+Авторизацию реализована с помощью JWT. 
+Пользователь отправляет POST запрос с login и password и получает ключ.
+
+Этот ключ возвращается в блоке HEAD.
+
+![Image of Arch](https://github.com/SlartiBartFast-art/job4j_tracker/blob/master/image/Screenshot_4.jpg)
+
+3. Регистрация URL.
+
+Поле того, как пользователь зарегистрировал свой сайт он может отправлять на сайт ссылки
+и получать преобразованные ссылки.
+
+Отправляем URL. POST /convert
+Получаем. Ответ от сервера - уникальный код.
+
+![Image of Arch](https://github.com/SlartiBartFast-art/job4j_tracker/blob/master/image/Screenshot_5.jpg)
+
+Ключ 4b7a2a11 ассоциирован с URL в БД
+
+4. Переадресация. Выполняется без авторизации.
+
+Когда сайт отправляет ссылку с кодом в ответ можно вернуть ассоциированный адрес и статус 302.
+Ответ от сервера в заголовке. HTTP CODE - 302 REDIRECT URL
+
+GET /redirect/УНИКАЛЬНЫЙ_КОД
+
+![Image of Arch](https://github.com/SlartiBartFast-art/job4j_tracker/blob/master/image/Screenshot_6.jpg)
+
+5. Статистика.
+
+В сервисе считается количество вызовов каждого адреса.
+
+По сайту можно получить статистку всех адресов и количество вызовов этого адреса.
+
+Опишем вызовы. GET /statistic
+
+![Image of Arch](https://github.com/SlartiBartFast-art/job4j_tracker/blob/master/image/Screenshot_1.jpg)
+
+Ответ от сервера JSON.
+
+
+
+
--- db/schema.sql
+++ db/schema.sql
@@ -1,50 +1,34 @@
-create table authoritys
+create table person
 (
-    id        serial primary key,
-    authority varchar(255)
+    id       serial primary key,
+    username varchar(255),
+    password varchar(255),
+    status   boolean default false
 );
 
-insert into authoritys (authority)
-values ('ROLE_USER');
-insert into authoritys (authority)
-values ('ROLE_ADMIN');
-create table users
+insert into person (username, password)
+values ('admin', '$2a$10$BSmGhQ2XVFOD7YWDOjBTyOHVoOzhA1yw69Pp2fEYXqP73.YQgGiwu');
+insert into person (username, password)
+values ('admin1', '$2a$10$LAmXtybMFiDujHkTXdN5.e3CpZDiPVV/egC.L4/NpWyu8Hhnksj32');
+
+create table url
 (
-    id           serial primary key,
-    enabled      boolean,
-    password     varchar(255),
-    username     varchar(255),
-    authority_id int references authoritys (id)
+    encode_url  varchar(255) primary key,
+    address_url varchar(255),
+    total       int,
+    person_id   int references person (id)
 );
 
-insert into users (username, enabled, password, authority_id)
-values ('Ivan Sobolev', true, '$2a$10$TqfYcEWuXxoY6L/aAz2X8egz47vmbtzqAH3PrnWF2gq2vfo7hmbHW',
-        (select id from authoritys where authority = 'USER'));
-insert into users (username, enabled, password, authority_id)
-values ('Svetlana Donovan', true, '$2a$10$HiWxVMtZjCOHyk4ktTzeQee7TR/BBrpnEmtORIi32N6Fh6NBiFpA.',
-        (select id from authoritys where authority = 'USER'));
-insert into users (username, enabled, password, authority_id)
-values ('Sergei Shirokov', true, '$2a$10$CzztUARq9leM95pmfI70OOT.qOrmxqQF.idwN1AOmpKRAMyzkjJsm',
-        (select id from authoritys where authority = 'USER'));
-insert into users (username, enabled, password, authority_id)
-values ('root', true, '$2a$10$NLzdxS.HApEfFa9M1P6sN.qbqDE3trwQnJzkjYBGmGjdqIMu9/dbi',
-        (select id from authoritys where authority = 'ADMIN'));
+insert into url (encode_url, address_url, total, person_id)
+values ('374f6556', 'https://job4j.ru/profile/exercise/106/task-view/532', 4,
+        (select id from person where id = 1));
+insert into url (encode_url, address_url, total, person_id)
+values ('38f1dba7e', 'https://job4j.ru/profile/exercise/104/task-view/792/solutionId/260783', 1,
+        (select id from person where id = 1));
+insert into url (encode_url, address_url, total, person_id)
+values ('21ec382d', 'https://www.baeldung.com/hibernate-identifiers', 1,
+        (select id from person where id = 1));
+insert into url (encode_url, address_url, total, person_id)
+values ('4b7a2a11', 'https://docs.oracle.com/javase/8/docs/api/java/util/HashMap.html#put-K-V-', 0,
+        (select id from person where id = 1));
 
-create table posts
-(
-    id          serial primary key,
-    created     timestamp without time zone not null default now(),
-    description varchar(255),
-    name        varchar(255),
-    user_id     int references users (id)
-);
-
-insert into posts (description, name, user_id)
-values ('Продам велосипед, новый, горный, 25 скоростей Schimano',
-        'Продам велосипед', 1);
-insert into posts (description, name, user_id)
-values ('Куплю или приму в дар мотоцикл, б/у, находу',
-        'Куплю мотоцикл', 2);
-insert into posts (description, name, user_id)
-values ('Требования Java, JS, HTML, PostgreSQL, Git, Linux',
-        'Вакансия на стажера', 3);
